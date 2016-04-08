3bdd9b5f8de91468abf60a4f71fd1703c477159f
Faregon <kunobelin@yahoo.com>
2016-04-09 00:28:58 +0200

    initial commit

---
+++ SecurityMandatory2/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Web;
+using System.Data.Entity;
+using SecurityMandatory2.Models;
+
+namespace SecurityMandatory2.Infrastructure
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser> 
+    {
+        public AppIdentityDbContext() : base("IdentityDb") { }
+
+        static AppIdentityDbContext()
+        {
+            Database.SetInitializer(new IdentityDbInit());
+        }
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+    }
+
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+    {
+        protected override void Seed(AppIdentityDbContext context)
+        {
+            PerformInitialSetup(context);
+            base.Seed(context);
+        }
+        public void PerformInitialSetup(AppIdentityDbContext context)
+        {
+
+        }
+    }
+}
