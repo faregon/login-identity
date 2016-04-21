394b162c7b1440367af6ca4364a50ad721204b44
Faregon <kunobelin@yahoo.com>
2016-04-21 13:34:30 +0200

    regex for username

--- SecurityMandatory2/Models/UserViewModels.cs
+++ SecurityMandatory2/Models/UserViewModels.cs
@@ -8,22 +8,24 @@ using System.Web;
 namespace SecurityMandatory2.Models
 {
 
-        public class CreateModel
-        {
-            [Required]
-            public string Name { get; set; }
-            [Required]
-            public string Email { get; set; }
-            [Required]
-            public string Password { get; set; }
-        }
-        public class LoginModel
-        {
-            [Required]
-            public string Name { get; set; }
-            [Required]
-            public string Password { get; set; }
-        }
+    public class CreateModel
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$")]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+    public class LoginModel
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$")]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
     public class RoleEditModel
     {
         public AppRole Role { get; set; }
