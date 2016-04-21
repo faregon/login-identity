09f61a285d422ee7ce6958f613d5d900357af7c4
Faregon <kunobelin@yahoo.com>
2016-04-21 12:28:42 +0200

    more password restrictions

--- SecurityMandatory2/Infrastructure/CustomPasswordValidator.cs
+++ SecurityMandatory2/Infrastructure/CustomPasswordValidator.cs
@@ -8,11 +8,22 @@ namespace SecurityMandatory2.Infrastructure
     {
         public override async Task<IdentityResult> ValidateAsync(string pass)
         {
+            
             IdentityResult result = await base.ValidateAsync(pass);
             if (pass.Contains("12345"))
             {
                 var errors = result.Errors.ToList();
                 errors.Add("Password cannot contain numeric sequnces");
+                result = new IdentityResult(errors);
+            
+            }else if (pass.ToUpper().Contains("ABCDE"))
+            {
+                var errors = result.Errors.ToList();
+                errors.Add("Password cannot contain alphanumeric sequences f.x. 'abcd'");
+                result = new IdentityResult(errors);
+            }else if(pass.ToUpper().Contains("ASDFG")) {
+                var errors = result.Errors.ToList();
+                errors.Add("password cannot contain keyboard sequneces");
                 result = new IdentityResult(errors);
             }
             return result;
